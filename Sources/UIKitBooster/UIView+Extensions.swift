2cd8f69017931eff5552866ad62e2cf486bd946f
GG <rkrudtls@gmail.com>
2023-12-28 19:48:59 +0900

    Init

---
+++ Sources/UIKitBooster/UIView+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by HappyDuck on 12/28/23.
+//
+
+import UIKit
+
+extension UIView {
+    public func someFunction() {
+        self.backgroundColor = .systemRed
+    }
+}
